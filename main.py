e00918005850ec297e325a79ea734cfe4ba319b0
Luy_ <choyanluy@gmail.com>
2024-12-12 12:42:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import flet as ft
 
 
 def principal(page: ft.Page):
-    page.title = 'Lista de Tarefas'
-    page.window.width = 500
-    page.window.height = 700
+    page.title = 'Lista de Tarefas' # titulo do app
+    page.window.width = 500 # O app abre com essa largura
+    page.window.min_width = 500 # Largura minima do app (nao diminui a largura mais do que esse valor)
+    page.window.max_width = 700 # Largura maxima do app (nao aumenta a largura mais do que esse valor)
+    
+    page.window.height = 700 # Abre o app com essa altura
+    page.window.max_height = 900  # Altura maxima do app (nao aumenta a altura mais do que esse valor)
+    page.window.min_height = 700 # Altura minima do app (nao diminui a altura mais do que esse valor)
     page.horizontal_alignment = 'center'
 
     # cria a tarefa
@@ -42,7 +47,7 @@ def principal(page: ft.Page):
             caixa_texto.focus() # caixa de texto com foco
             caixa_texto.update()
 
-    botao_add = ft.ElevatedButton('Adicionar Tarefa', on_click= adiciona_tarefa, )
+    botao_add = ft.ElevatedButton('Adicionar Tarefa', on_click= adiciona_tarefa, color= ft.Colors.CYAN, bgcolor= ft.Colors.WHITE,)
     caixa_texto = ft.TextField(label= 'O que vamos fazer hoje?')
     alerta_confirmar = ft.AlertDialog(
         modal= True,
@@ -52,7 +57,6 @@ def principal(page: ft.Page):
             ft.ElevatedButton('Cancelar', on_click= fechar_alerta)
         ]
     )
-    
     # renderiza na tela
     ft.SafeArea(
         page.add(
